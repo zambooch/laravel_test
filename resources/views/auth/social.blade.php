5b381a8379902bbd45b51915972869ecd1559f44
alekseysenchev <cemcen@mail.ru>
2018-01-18 14:23:08 +0500

    fix

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -3,7 +3,7 @@
 <div class="text-center margin-bottom-20" id="uLogin"
      data-ulogin="display=panel;theme=flat;fields=first_name,last_name,email,nickname,photo,country;
                              providers=facebook,vkontakte,odnoklassniki,mailru;hidden=other;
-                             redirect_uri={{ 'http://0.0.0.0:8080'}}/ulogin;mobilebuttons=0;">
+                             redirect_uri={{ env('APP_URL')}}/ulogin;mobilebuttons=0;">
 </div>
 
 @section('js')
